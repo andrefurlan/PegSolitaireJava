b333a5fe842f54f7c0f2a98dde7757b7252c3cfa
andrefurlan <andrefurlan@live.com>
2014-01-19 09:08:45 -0800

    board as object

--- PegSolitaire/src/Model/Board.java
+++ PegSolitaire/src/Model/Board.java
@@ -14,62 +14,24 @@ import java.util.List;
 
 public class Board {
 
-		private static int[] solvedBoard = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		private static List<Board> nextBoards;
+	
+	private Board createBoard(int[] board){
 		
-		public static int[] solve(int[] initialBoard) {
+		
+		
+		return null; 
+	}
+	//public board
+		//private static int[] solvedBoard = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		
+		//public Board makeJump(board) {
 			
-			 //int[] solvedBoard = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
-			
-						
-			 if (won(initialBoard) == true) {
-				 solvedBoard = initialBoard;
-			 }	else {
-				 solvedBoard = solveListOfBoards(initialBoard);
-			 	}
-			 	
-			return solvedBoard;
-		}
+		//	return board;
+		//}
 
 		
 		
 		
-		private static int[] solveListOfBoards(int[] initialBoard) {
-			// TODO Auto-generated method stub
-			int[] solvedBoard = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
-			
-			nextBoards = new ArrayList<Board>();
-			getNextBoards(initialBoard);
-			
-			return solvedBoard;
-		}
-
-
-
-
-		private static Object getNextBoards(int[] initialBoard) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-
-
-
-		private static boolean won(int[] initialBoard) {
-			
-			int soma = 0;
-			int i = 0;
-			while(i < initialBoard.length) {
-				soma += initialBoard[i];
-				i++;
-			}
-			
-			return (soma == 1);
-		}
-		
-		
-		
-		
-		
+					
 	}
 	
--- PegSolitaire/src/Model/PegSolitaire.java
+++ PegSolitaire/src/Model/PegSolitaire.java
@@ -7,24 +7,81 @@
  */
 package Model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-	public class PegSolitaire {
-		
-		
-		public static int[] initialBoard = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-		//public static int[] initialBoard = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		
-		public static void main(String[] args) {
-			int[] solvedBoard = Board.solve(initialBoard);
-			for (int i = 0; i < initialBoard.length; i++) {
-				System.out.println(solvedBoard[i]);	
-			}	
-			 
-		 }
-		 
 
-		 
-		 
+public class PegSolitaire {
+		
+	
+	//Initial Board
+	public static final int[] initialBoard = 
+		{0, 
+		1, 1, 
+		1, 1, 1, 
+		1, 1, 1, 1, 
+		1, 1, 1, 1, 1};
+	public static List<int[]> nextBoards;
+	//public static int[] solvedBoard = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+		
+	public static void main(String[] args) {
+		
+		int[] solvedBoard = solve(initialBoard);
+		for (int i = 0; i < initialBoard.length; i++) {
+			System.out.println(solvedBoard[i]);	
+		}	
+	 }
+		
+	public static int[] solve(int[] board) {
+			
+		int[] solvedBoard = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+		 if (won(board) == true) {
+			 solvedBoard = board;
+		 }	else {
+			 solvedBoard = solveListOfBoards(board);
+		 	}	 	
+			return solvedBoard;
+		}
+
+		
+		private static int[] solveListOfBoards(int[] initialBoard) {
+			// TODO Auto-generated method stub
+			int[] solvedBoard = {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1};
+			List<int[]> nextBoards = new ArrayList<int[]>();
+			//Board nextBoard = {1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+			//nextBoards = new List<>();
+			//getNextBoards(initialBoard);
+			
+			//nextBoards.add(board.nextBoard);
+			
+			return solvedBoard;
+		}
+
+
+
+
+		private static Object getNextBoards(int[] initialBoard) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		private static boolean won(int[] initialBoard) {
+			
+			int soma = 0;
+			int i = 0;
+			while(i < initialBoard.length) {
+				soma += initialBoard[i];
+				i++;
+			}
+			
+			return (soma == 1);
+		}
+		
+	
 		
 	}
 	
